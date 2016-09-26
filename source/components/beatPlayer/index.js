5e166105eca2a7e83f848f10db0bf69acb320e3b
Paul Davidowitz <pdavidow@fastmail.fm>
2016-09-26 00:02:55 -0400

    want to simulate button click...

--- source/components/beatPlayer/index.js
+++ source/components/beatPlayer/index.js
@@ -19,7 +19,7 @@ export default (React) => {
       render () {
         return (
           <div className="beatPlayer">
-            <button type="submit" id="playButton" onClick={onPlay}>=== Play Beat ===</button>
+            <button type="submit" id="playButton" ref="playButtonRef" onClick={onPlay}>=== Play Beat ===</button>
           </div>
         );
       }
