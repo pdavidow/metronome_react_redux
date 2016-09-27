dd68404a0d42328266336c7a970082b0ea339451
Paul Davidowitz <pdavidow@fastmail.fm>
2016-09-27 15:18:19 -0400

    remove redundant tests

--- source/test/models/tick/index.js
+++ source/test/models/tick/index.js
@@ -9,64 +9,6 @@ import {
 ////////////////////////////////////
 
 test('Tick model', nestOuter => {
-  nestOuter.test('...The four types', nestInner => {
-    nestInner.test('......rh', assert => {
-      const msg = 'Tick is rh';
-
-      const tick = {
-        isRH: true,
-        isLH: false
-      };
-
-      const actual = isTick_Rh(tick);
-      const expected = true;
-
-      assert.equal(actual, expected, msg);
-      assert.end();
-    });
-    nestInner.test('......lh', assert => {
-      const msg = 'Tick is lh';
-
-      const tick = {
-        isRH: false,
-        isLH: true
-      };
-
-      const actual = isTick_Lh(tick);
-      const expected = true;
-
-      assert.equal(actual, expected, msg);
-      assert.end();
-    });
-    nestInner.test('......rh & lh', assert => {
-      const msg = 'Tick is rh & lh';
-
-      const tick = {
-        isRH: true,
-        isLH: true
-      };
-
-      const actual = isTick_RhLh(tick);
-      const expected = true;
-
-      assert.equal(actual, expected, msg);
-      assert.end();
-    });
-    nestInner.test('......background', assert => {
-      const msg = 'Tick is background';
-
-      const tick = {
-        isRH: false,
-        isLH: false
-      };
-
-      const actual = isTick_Background(tick);
-      const expected = true;
-
-      assert.equal(actual, expected, msg);
-      assert.end();
-    });
-  });
   nestOuter.test('...The four types are mutually exclusive', nestInner => {
     nestInner.test('......rh', assert => {
       const msg = 'Tick is exclusively rh';
