14ff8fa140e97c43f68f74d6d007714a15f38cc1
Paul Davidowitz <pdavidow@fastmail.fm>
2016-11-29 15:07:48 -0500

    refactor test/browser/app/index.js

--- source/test/browser/app/index.js
+++ source/test/browser/app/index.js
@@ -7,19 +7,18 @@ import {Simulate as simulate} from 'react-addons-test-utils';
 import {
   getDomNode,
   getElementBySelector,
-  setStore,
-  waitInAudioTime,
-  embeddedAudioTest
+  setStore
 } from '../utils';
+import {
+  audioTestStart,
+  audioTestEnd,
+} from '../../../models/audio/destination';
 ////////////////////////////////////
 
-// Careful: React may replace the element it is modifying, instead of changing it in place.
-// So always retreive the element, instead of keeping a pointer to it.
 const getPlayButton = ({domNode}) => getElementBySelector({domNode, selector: '#playButton'});
 const getBeatsComponent = ({domNode}) => getElementBySelector({domNode, selector: '#beatsFieldset'});
 const getMetronomeSettingComponent = ({domNode}) => getElementBySelector({domNode, selector: '#metronomeSettingFieldset'});
 const getPlayerSettingComponent = ({domNode}) => getElementBySelector({domNode, selector: '.playerSetting'});
-
 
 test('App', nestOuter => {
   nestOuter.test('...Beats Component, MetronomeSetting, PlayerSetting Components should disable during play', nestInner => {
@@ -43,75 +42,65 @@ test('App', nestOuter => {
       assert.deepEqual(actual, expected, msg);
       assert.end();
     });
-    nestInner.test('......Beats Component, MetronomeSetting, PlayerSetting Components should be disabled during play', (assert) => {
+    nestInner.test('......Beats Component, MetronomeSetting, PlayerSetting Components should be disabled during play', async(assert) => {
+      audioTestStart();
       const msg = 'Should be disabled';
 
-      const store = setStore({beats: [{rh: 1, lh: 1}]});
+      const beats = [{rh: 1, lh: 1}];
+      const metronomeSetting = {classicTicksPerMinute: 60, classicTicksPerBeat: 1};
+      const isLooping = false;
+      const store = setStore({beats, metronomeSetting, isLooping});
       const domNode = getDomNode({store});
 
-      embeddedAudioTest.componentsDisabledDuringPlay = async({audioContext, oscillator}) => {
-        embeddedAudioTest.componentsDisabledDuringPlay = null;
-
-        const startTime = audioContext.currentTime;
-        oscillator.start(startTime);
-        oscillator.stop(startTime + 0.5); // sec
-
-        const waitTime = 0.1; // sec
-        waitInAudioTime({waitTime, audioContext, startTime});
-        await sleep(1) /* msec */; // for some reason, must sleep something
-
-        const actual = {
-          beats: getBeatsComponent({domNode}).hasAttribute('disabled'),
-          metronomeSetting: getMetronomeSettingComponent({domNode}).hasAttribute('disabled'),
-          playerSetting: getPlayerSettingComponent({domNode}).hasAttribute('disabled')
-        };
-
-        const expected = {
-          beats: true,
-          metronomeSetting: true,
-          playerSetting: true
-        };
-
-        assert.deepEqual(actual, expected, msg);
-        assert.end();
-      };
       simulate.click(getPlayButton({domNode}));
+      await sleep(250) /* msec */;
+
+      const actual = {
+        beats: getBeatsComponent({domNode}).hasAttribute('disabled'),
+        metronomeSetting: getMetronomeSettingComponent({domNode}).hasAttribute('disabled'),
+        playerSetting: getPlayerSettingComponent({domNode}).hasAttribute('disabled')
+      };
+
+      const expected = {
+        beats: true,
+        metronomeSetting: true,
+        playerSetting: true
+      };
+
+      assert.deepEqual(actual, expected, msg);
+      assert.end();
+
+      await sleep(3000) /* msec */;
+      audioTestEnd();
     });
-    nestInner.test('......Beats Component, MetronomeSetting, PlayerSetting Components should be enabled after play', (assert) => {
+    nestInner.test('......Beats Component, MetronomeSetting, PlayerSetting Components should be enabled after play', async(assert) => {
+      audioTestStart();
       const msg = 'Should be enabled';
 
-      const store = setStore({beats: [{rh: 1, lh: 1}]});
+      const beats = [{rh: 1, lh: 1}];
+      const metronomeSetting = {classicTicksPerMinute: 240, classicTicksPerBeat: 1};
+      const isLooping = false;
+      const store = setStore({beats, metronomeSetting, isLooping});
       const domNode = getDomNode({store});
 
-      embeddedAudioTest.componentsDisabledDuringPlay = async({audioContext, oscillator}) => {
-        if (oscillator.onended == undefined) return; // want the spacer
-        embeddedAudioTest.componentsDisabledDuringPlay = null;
-
-        oscillator.connect(audioContext.createAnalyser());
-        const startTime = audioContext.currentTime;
-        oscillator.start(startTime);
-        oscillator.stop(startTime + 0.5); // sec
-
-        const waitTime = 0.6; // sec
-        waitInAudioTime({waitTime, audioContext, startTime});
-        await sleep(1) /* msec */; // for some reason, must sleep something
-
-        const actual = {
-          beats: getBeatsComponent({domNode}).hasAttribute('disabled'),
-          metronomeSetting: getMetronomeSettingComponent({domNode}).hasAttribute('disabled'),
-          playerSetting: getPlayerSettingComponent({domNode}).hasAttribute('disabled')
-        };
-
-        const expected = {
-          beats: false,
-          metronomeSetting: false,
-          playerSetting: false
-        };
-
-        assert.deepEqual(actual, expected, msg);
-        assert.end();
-      };
       simulate.click(getPlayButton({domNode}));
+      await sleep(3000) /* msec */;
+
+      const actual = {
+        beats: getBeatsComponent({domNode}).hasAttribute('disabled'),
+        metronomeSetting: getMetronomeSettingComponent({domNode}).hasAttribute('disabled'),
+        playerSetting: getPlayerSettingComponent({domNode}).hasAttribute('disabled')
+      };
+
+      const expected = {
+        beats: false,
+        metronomeSetting: false,
+        playerSetting: false
+      };
+
+      assert.deepEqual(actual, expected, msg);
+      assert.end();
+      audioTestEnd();
     });
   });
 });
